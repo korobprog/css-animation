f876aaf869c20d1b745549736d655d703a4bcd4f
Максим Коробков <maksimkorobkov@iMac-Maksim.local>
2023-12-23 19:31:04 +1000

    finish

--- src/js/Collapsible.js
+++ src/js/Collapsible.js
@@ -1,6 +1,6 @@
 export default class Collapsible {
    button = document.createElement("button");
-   buttonText = document.createTextNode("Collapse");
+   buttonText = document.createTextNode("collapse");
    buttonClasses = this.button.classList;
    constructor() {
       this.buttonClasses.add("collapsible");
@@ -8,17 +8,27 @@ export default class Collapsible {
       this.content = document.createElement("div");
       const contentText = document.createTextNode(
          "В рамках реализации большого портала вам необходимо сделать виджет collapsible-контейнеров, который может разворачиваться и сворачиваться."
-      );
 
+      );
       const contentClasses = this.content.classList;
       contentClasses.add("content");
       this.content.appendChild(contentText);
    }
    init() {
       this.addPend();
+      this.onClick();
    }
    addPend() {
       document.body.appendChild(this.button);
       document.body.appendChild(this.content);
+   }
+   onClick() {
+      this.button.addEventListener("click", () => {
+         if (this.content.style.display === "block") {
+            this.content.style.display = "none";
+         } else {
+            this.content.style.display = "block";
+         }
+      });
    }
 }
